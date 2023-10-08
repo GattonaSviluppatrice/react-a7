f77186ea6fdad04860e1c9ddc23da6755a535320
Alessia Mori <49532458+GattonaSviluppatrice@users.noreply.github.com>
2023-10-08 21:47:21 +0200

    1

---
+++ HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Benvenuto sulla Homepage</h1>
+      <p>Questa è la pagina principale del nostro sito.</p>
+      {/* Aggiungi qui il contenuto aggiuntivo della tua homepage */}
+    </div>
+  );
+}
+
+export default HomePage;
